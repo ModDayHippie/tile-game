eb1d471d524673706dea69498cd0643b83242db1
Josh <48167631+ModDayHippie@users.noreply.github.com>
2021-11-04 16:36:18 -0400

    alpha
    made map bigger, added camera scrolling

--- settings.py
+++ settings.py
@@ -6,13 +6,15 @@ LIGHTGREY = (100, 100, 100)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
+BLUE = (0, 43, 255)
+BROWN =(107, 62, 4)
 
 # game window settings
 WIDTH = 640   # 16 * 64 or 32 * 32 or 64 * 16
 HEIGHT = 480  # 16 * 48 or 32 * 24 or 64 * 12
 FPS = 60
 TITLE = "Tilemap Demo"
-BGCOLOR = DARKGREY
+BGCOLOR = GREEN
 
 TILESIZE = 16
 GRIDWIDTH = WIDTH / TILESIZE
--- sprites.py
+++ sprites.py
@@ -56,3 +56,18 @@ class Fire(pg.sprite.Sprite):
         self.y = y
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
+
+#water is classified as 3 on the map.txt
+class Water(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+

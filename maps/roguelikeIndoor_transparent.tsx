5d68356cbee696dec61b98d9102cbcbd7b68b785
Josh <48167631+ModDayHippie@users.noreply.github.com>
2021-11-06 13:36:11 -0400

    playable alpha
    added collisions

---
+++ maps/roguelikeIndoor_transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="roguelikeIndoor_transparent" tilewidth="64" tileheight="64" tilecount="28" columns="7">
+ <image source="../img/Spritesheet/roguelikeIndoor_transparent.png" width="457" height="305"/>
+</tileset>

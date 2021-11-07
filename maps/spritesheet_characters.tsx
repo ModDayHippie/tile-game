8de9e08728844d9003d62a331b3f4c94308141a6
Josh <48167631+ModDayHippie@users.noreply.github.com>
2021-11-07 16:27:08 -0500

    B0.4
    added ak and a new shotgun sprite thats easier to see on dark backgrounds, made playable area around second house, added blood splatters to inidicate a human death(dead person sprite to be added soon)

---
+++ maps/spritesheet_characters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="spritesheet_characters" tilewidth="64" tileheight="64" tilecount="32" columns="8">
+ <image source="../img/Spritesheet/spritesheet_characters.png" width="512" height="256"/>
+</tileset>
